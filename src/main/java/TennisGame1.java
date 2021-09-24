12a9b8fe9dca05a6d5a89e1bbac886b80bc501ef
Johana catalina rios torres <jcriostorres@gmail.com>
2021-09-24 00:45:21 -0500

    Firts commit y correcion de variables de la clase TennisGame1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -12,18 +12,18 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(this.player1Name) )
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+
+        if (scorePlayer1 == scorePlayer2)
         {
-            switch (m_score1)
+            switch (scorePlayer1)
             {
                 case 0:
                         score = "Love-All";
@@ -40,36 +40,42 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
+            int minusResult = scorePlayer1 - scorePlayer2;
+            if (minusResult==1) score ="Advantage "+ this.player1Name;
+            else if (minusResult ==-1) score ="Advantage "+this.player2Name;
+            else if (minusResult>=2) score = "Win for "+this.player1Name;
             else score ="Win for player2";
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                score = getResult(score, i);
             }
+        }
+        return score;
+    }
+
+    private String getResult(String score, int i) {
+        int tempScore;
+        if (i ==1) tempScore = scorePlayer1;
+        else { score +="-"; tempScore = scorePlayer2;}
+        switch(tempScore)
+        {
+            case 0:
+                score +="Love";
+                break;
+            case 1:
+                score +="Fifteen";
+                break;
+            case 2:
+                score +="Thirty";
+                break;
+            case 3:
+                score +="Forty";
+                break;
         }
         return score;
     }
